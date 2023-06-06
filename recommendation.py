497154c590c0272f668227ff18472906f120b60a
Anuj Malviya <75200824+Anuj-malviya0@users.noreply.github.com>
2023-06-06 11:28:43 +0530

    Update recommendation.py
    added memory variable to fix the deployment error

--- recommendation.py
+++ recommendation.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import modin.pandas as pd
+import os
+os.environ["MODIN_MEMORY"] = "8g"
 from statistics import harmonic_mean
 from langdetect import detect
 from sklearnex import patch_sklearn
